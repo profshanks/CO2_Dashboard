6bbcb7c14ebf4cc9362ac150e3786b68ca14acf3
Sam Shanks <sshanks@fastmail.fm>
2023-12-21 09:43:32 -0600

    d

--- docs/co2_dash.js
+++ docs/co2_dash.js
@@ -15,7 +15,7 @@ async function startApplication() {
   self.pyodide.globals.set("sendPatch", sendPatch);
   console.log("Loaded!");
   await self.pyodide.loadPackage("micropip");
-  const env_spec = ['https://github.com/profshanks/CO2_Dashboard/raw/main/bokeh-3.1.1-py3-none-any.whl', 'https://cdn.holoviz.org/panel/1.2.3/dist/wheels/panel-1.2.3-py3-none-any.whl', 'pyodide-http==0.2.1', 'pandas', 'holoviews']
+  const env_spec = ['https://github.com/profshanks/CO2_Dashboard/raw/main/docs/bokeh-3.1.1-py3-none-any.whl', 'https://cdn.holoviz.org/panel/1.2.3/dist/wheels/panel-1.2.3-py3-none-any.whl', 'pyodide-http==0.2.1', 'pandas', 'holoviews']
   for (const pkg of env_spec) {
     let pkg_name;
     if (pkg.endsWith('.whl')) {
